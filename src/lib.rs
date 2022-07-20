a760f844c5a5814841623abd3145f8c97b08156b
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2022-07-20 17:42:34 +0200

    Move custom-format package to workspace root

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+#![no_std]
+
+use core::fmt;
+
+/// Trait for custom formatting
+pub trait CustomFormat {
+    fn fmt(&self, f: &mut fmt::Formatter, spec: &str) -> fmt::Result;
+}
+
+/// Wrapper for custom formatting via its [`Display`](core::fmt::Display) trait
+pub struct CustomFormatter<'a, T> {
+    /// Value to format
+    value: &'a T,
+    /// Format specifier
+    spec: &'static str,
+}
+
+impl<'a, T> CustomFormatter<'a, T> {
+    /// Construct a new [`CustomFormatter`] value
+    pub fn new(value: &'a T, spec: &'static str) -> Self {
+        Self { value, spec }
+    }
+}
+
+impl<T: CustomFormat> fmt::Display for CustomFormatter<'_, T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        CustomFormat::fmt(self.value, f, self.spec)
+    }
+}
+
+pub use custom_format_macros::*;
